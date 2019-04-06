2fa0bfa3920fc0d6add3b620c917cb1ea86352d4
luisgustavogomes <luisgustavogomes@outlook.com>
2019-04-06 18:57:28 -0300

    Salvando item arquivo json no banco

---
+++ CasaDoCodigo/DataService.cs
@@ -0,0 +1,56 @@
+﻿using CasaDoCodigo.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo
+{
+    public class DataService : IDataService
+    {
+        private readonly ApplicationContext _context;
+
+        public DataService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public void InicializaDB()
+        {
+            _context.Database.EnsureCreated();
+            //var json = File.ReadAllText("livros.json");
+            //var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+            //foreach (var item in livros)
+            //{
+            //    _context.Set<Produto>().Add(new Produto(item.Codigo, item.Nome, item.Preco));
+            //}
+            //_context.SaveChanges();
+        }
+
+        //Não utiliza migrations
+        //serviceProvider.GetService<ApplicationContext>().Database.EnsureCreated();
+
+        //Utiliza migrations
+        //serviceProvider.GetService<ApplicationContext>().Database.Migrate();
+
+        class Livro
+        {
+            public string Codigo { get; set; }
+            public string Nome { get; set; }
+            public decimal Preco { get; set; }
+
+            //public void ex()
+            //{
+            //    var json = File.ReadAllText("livros.json");
+            //    var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+            //    foreach (var item in livros)
+            //    {
+            //        _context.Set<Produto>().Add(new Produto(item.Codigo, item.Nome, item.Preco));
+            //    }
+            //    _context.SaveChanges();
+            //}
+        }
+    }
+}
--- CasaDoCodigo/Startup.cs
+++ CasaDoCodigo/Startup.cs
@@ -29,10 +29,12 @@ namespace CasaDoCodigo
             services.AddDbContext<ApplicationContext>(options =>
                 options.UseSqlServer(connectionString)
             );
+
+            services.AddTransient<IDataService, DataService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -52,6 +54,9 @@ namespace CasaDoCodigo
                     name: "default",
                     template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
+
+
+            serviceProvider.GetService<IDataService>().InicializaDB();
         }
     }
 }
