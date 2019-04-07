0a2f16edad46d3a665348ba80d7a80af4c14e7e9
luisgustavogomes <luisgustavogomes@outlook.com>
2019-04-07 09:25:39 -0300

    Repository

--- CasaDoCodigo/DataService.cs
+++ CasaDoCodigo/DataService.cs
@@ -1,56 +1,45 @@
 ﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Repositories;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using static CasaDoCodigo.Repositories.ProdutoRepository;
 
 namespace CasaDoCodigo
 {
     public class DataService : IDataService
     {
         private readonly ApplicationContext _context;
+        private readonly IProdutoRepository _produtoRepository;
 
-        public DataService(ApplicationContext context)
+        public DataService(ApplicationContext context, IProdutoRepository produtoRepository)
         {
             _context = context;
+            _produtoRepository = produtoRepository;
         }
 
+        /// <summary>
+        /// Não utiliza migrations - serviceProvider.GetService<ApplicationContext>().Database.EnsureCreated();
+        /// Utiliza migrations - serviceProvider.GetService<ApplicationContext>().Database.Migrate();
+        /// </summary>
         public void InicializaDB()
         {
             _context.Database.EnsureCreated();
-            //var json = File.ReadAllText("livros.json");
-            //var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
-            //foreach (var item in livros)
-            //{
-            //    _context.Set<Produto>().Add(new Produto(item.Codigo, item.Nome, item.Preco));
-            //}
-            //_context.SaveChanges();
+            _produtoRepository.SaveProdutos(GetLivros());
         }
 
-        //Não utiliza migrations
-        //serviceProvider.GetService<ApplicationContext>().Database.EnsureCreated();
-
-        //Utiliza migrations
-        //serviceProvider.GetService<ApplicationContext>().Database.Migrate();
-
-        class Livro
+        private static List<Livro> GetLivros()
         {
-            public string Codigo { get; set; }
-            public string Nome { get; set; }
-            public decimal Preco { get; set; }
-
-            //public void ex()
-            //{
-            //    var json = File.ReadAllText("livros.json");
-            //    var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
-            //    foreach (var item in livros)
-            //    {
-            //        _context.Set<Produto>().Add(new Produto(item.Codigo, item.Nome, item.Preco));
-            //    }
-            //    _context.SaveChanges();
-            //}
+            var json = File.ReadAllText("livros.json");
+            var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+            return livros;
         }
+
+        
+
+        
     }
 }
---
+++ CasaDoCodigo/Repositories/ProdutoRepository.cs
@@ -0,0 +1,35 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly ApplicationContext _context;
+
+        public ProdutoRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public void SaveProdutos(List<Livro> livros)
+        {
+            foreach (var item in livros)
+            {
+                _context.Set<Produto>().Add(new Produto(item.Codigo, item.Nome, item.Preco));
+            }
+            _context.SaveChanges();
+        }
+
+        public class Livro
+        {
+            public string Codigo { get; set; }
+            public string Nome { get; set; }
+            public decimal Preco { get; set; }
+
+        }
+    }
+}
